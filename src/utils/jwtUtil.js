ae7805d873fc8dfee53c20a0426d0a43b0269e17
Maksym <159819659+rybmks@users.noreply.github.com>
2025-03-20 02:23:00 +0200

    Dev (#1)
    add user registration

--- src/utils/jwtUtil.js
+++ src/utils/jwtUtil.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY || "default_secret_key"; 
 
 function generateToken(payload, expiresIn = "6h") {
+    if (typeof payload === "number") {
+        payload = { id: payload }; 
+    }
+    
     return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
