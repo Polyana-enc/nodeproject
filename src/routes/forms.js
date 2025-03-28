11ec3f443b0e95d643c8549dcd40aeefface56de
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-03-28 23:27:59 +0200

    form & user repositories formatted and documentation added

--- src/routes/forms.js
+++ src/routes/forms.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const authMiddleware = require("../middleware/auth.js");
+const router = express.Router();
 const {
   getFormById,
   createForm,
@@ -6,10 +8,8 @@ const {
   deleteFormByUserId,
   getFormByUserId,
   getInfoById,
-  getInfoByUserId
+  getInfoByUserId,
 } = require("../controllers/form_controller.js");
-const authMiddleware = require("../middleware/auth.js");
-const router = express.Router();
 
 router.post("/form", authMiddleware, createForm);
 
@@ -26,8 +26,7 @@ router.get("/form/:form_id/:type", getInfoById);
 
 // DELETE http://example.com/form
 // Authorization: token.user_id.1
-router.delete("/form", authMiddleware, deleteFormByUserId)
-router.delete("/form/:form_id", deleteFormById)
-
+router.delete("/form", authMiddleware, deleteFormByUserId);
+router.delete("/form/:form_id", deleteFormById);
 
 module.exports = router;
--- src/service/form_service.js
+++ src/service/form_service.js
@@ -2,8 +2,8 @@ const { get_form_by_id, get_form_by_user_id } = require("../repository/form_repo
 
 /**
  *
- * @param {*} id form id
- * @param {'public' | 'private'} type info type
+ * @param {number} id 
+ * @param {'public' | 'private'} type 
  * @returns
  */
 async function getFormById(id, type) {
